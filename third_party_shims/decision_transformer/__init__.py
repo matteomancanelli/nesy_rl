d5d2af6daaa802aa2d114775c4e00078e94bf2d3
mancanelli <matteomancanelli@gmail.com>
2026-01-07 03:30:26 +0100

    add DT repo to submodules

--- third_party_shims/decision_transformer/__init__.py
+++ third_party_shims/decision_transformer/__init__.py
@@ -2,5 +2,5 @@ import sys
 from pathlib import Path
 
 ROOT = Path(__file__).resolve().parents[2]
-SUB = ROOT / "submodules" / "suffix-prediction"
+SUB = ROOT / "submodules" / "decision-transformer"
 sys.path.insert(0, str(SUB))
--- third_party_shims/nesy_suffix_prediction_dfa/__init__.py
+++ third_party_shims/nesy_suffix_prediction_dfa/__init__.py
@@ -4,5 +4,3 @@ from pathlib import Path
 ROOT = Path(__file__).resolve().parents[2]
 SUB = ROOT / "submodules" / "nesy-suffix-prediction-dfa"
 sys.path.insert(0, str(SUB))
-
-# optionally import common symbols to make it feel like a package
